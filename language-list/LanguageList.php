907b64f3ff8fe1829db74baf8070bd3d36050fa4
Olivier Odiet <111279035+oliodi83@users.noreply.github.com>
2024-06-21 19:41:31 +0100

    Complete: Language List

---
+++ language-list/LanguageList.php
@@ -0,0 +1,30 @@
+<?php
+
+function language_list(...$languages)
+{
+    return $languages;
+}
+
+function add_to_language_list($languages, $new_language)
+{
+    array_push($languages, $new_language);
+    
+    return $languages;
+}
+
+function prune_language_list($languages)
+{
+    array_shift($languages);
+
+    return $languages;
+}
+
+function current_language($languages)
+{
+    return $languages[0];
+}
+
+function language_list_length($languages)
+{
+    return count($languages);
+}
